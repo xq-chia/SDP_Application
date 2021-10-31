614b64c0223d17384685065dd585d044bf45c593
zachwong02 <wongzhizhen3@gmail.com>
2021-10-31 14:45:10 +0800

    new files added slight adjustments

--- src/main/java/com/assignment/sdp_application/marketing_main.java
+++ src/main/java/com/assignment/sdp_application/marketing_main.java
@@ -1,4 +1,48 @@
 package com.assignment.sdp_application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class marketing_main {
+
+    @FXML
+    private Button promotionsButton;
+    @FXML
+    private Button researchButton;
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void initialize(){
+
+        promotionsButton.setOnAction(e -> {
+
+        });
+        researchButton.setOnAction(e -> {
+
+        });
+
+
+
+    }
+    public void goSomewhere(ActionEvent e, String fxml, String title) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(fxml));
+        stage =(Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root,1080,640);
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 }
